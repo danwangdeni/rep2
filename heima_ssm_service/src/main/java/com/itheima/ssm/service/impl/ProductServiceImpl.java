9cf87223e90edac4dc30c958d9fbc73eff283ab4
zhengyang <zhengyang@wondersgroup.com>
2020-10-16 21:06:32 +0800

    第一次提交111

---
+++ heima_ssm_service/src/main/java/com/itheima/ssm/service/impl/ProductServiceImpl.java
@@ -0,0 +1,23 @@
+package com.itheima.ssm.service.impl;
+
+import com.itheima.ssm.dao.IProductDao;
+import com.itheima.ssm.domain.Product;
+import com.itheima.ssm.service.IproductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class ProductServiceImpl implements IproductService {
+
+    @Autowired
+    private IProductDao producDao;
+
+    @Override
+    public List<Product> findAll() throws Exception {
+        return producDao.findAll();
+    }
+}
